b1052f5185af448c5aa45015689f546840bb8441
Daniil S <sidorenkodk@gmail.com>
2023-02-23 01:30:18 +0300

    add: logger config + error handling func

--- src/auth/main.py
+++ src/auth/main.py
@@ -1,3 +1,6 @@
+import json
+import logging.config
+
 import uvicorn
 from fastapi import FastAPI
 
@@ -8,6 +11,9 @@ app = FastAPI()
 def main():
     return {'Message': 'Hello Dance'}
 
+
+logger_config_file = open('./config/logger.json')
+logging.config.dictConfig(json.load(logger_config_file))
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8080)
